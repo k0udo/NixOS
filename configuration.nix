93c22c531b9b9b90372818934c65390378d48873
k0udo <36237258+k0udo@users.noreply.github.com>
2024-05-09 00:48:16 -0700

    sddm preconfig

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,5 @@
+# config.nix
+
 { config, pkgs, ... }:
 
 {
@@ -44,10 +46,14 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
+
+  # SDDM Greeter
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.theme = "${import ./sddm-theme.nix {inherit pkgs;}}";
+  
+  # Enable the KDE Plasma Desktop Environment.
+  services.desktopManager.plasma6.enable = true;
   
   # Configure keymap in X11
   services.xserver = {
