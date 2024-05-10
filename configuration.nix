b26ef2779c0aab2f80ba14c14abe474e57640473
k0udo <36237258+k0udo@users.noreply.github.com>
2024-05-10 00:16:08 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -73,10 +73,8 @@
     pulse.enable = true;
   };
 
-  # Enable experimental features
   # nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Define a user account.
   users.users.jeff = {
     isNormalUser = true;
     description = "jeff";
@@ -95,32 +93,10 @@
     ];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-    environment.systemPackages = [
-
+    environment.systemPackages = with pkgs; [
   ];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
+  system.stateVersion = "23.11";
 }
